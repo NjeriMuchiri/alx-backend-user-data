20be9f152816c763f0a1e1f70faef47d75cd8b65
NjeriMuchiri <muchirinjeri3210@gmail.com>
2024-01-27 16:36:44 +0300

    session authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -8,7 +8,6 @@ class SessionAuth(Auth):
     """Class authentication"""
     user_id_by_session_id = {}
 
-
     def create_session(self, user_id: str = None) -> str:
         """Method for a session id for a user id"""
         if user_id is None or not isinstance(user_id, str):
@@ -19,3 +18,10 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Method that returns a User ID based on a Session ID"""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
