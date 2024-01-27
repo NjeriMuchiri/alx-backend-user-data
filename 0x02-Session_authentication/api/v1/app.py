1cf7439d51c543650dda90ba6fb1e260f9650081
NjeriMuchiri <muchirinjeri3210@gmail.com>
2024-01-27 15:42:37 +0300

    session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -6,6 +6,7 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -20,6 +21,15 @@ if AUTH_TYPE == "auth":
 elif AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+# elif AUTH_TYPE == "session_exp_auth":
+#     from api.v1.auth.session_exp_auth import SessionExpAuth
+#     auth = SessionExpAuth()
+# elif AUTH_TYPE == "session_db_auth":
+#     from api.v1.auth.session_db_auth import SessionDBAuth
+#     auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
